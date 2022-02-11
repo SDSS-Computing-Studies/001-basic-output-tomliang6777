7659351e7e13163ae4d5d8d33132f9c7a3ac66d0
tomliang6777 <toml788@deltalearns.ca>
2022-02-11 08:40:24 -0800

    done

---
+++ assignement 2.py
@@ -0,0 +1,1 @@
+print("Sample output task #2\nThis is my second program.\nIt uses \"two commands\"to display the output.")
---
+++ assignment 3.py
@@ -0,0 +1,1 @@
+print("This is the first sentence.\nTHis is the second sentence.")
